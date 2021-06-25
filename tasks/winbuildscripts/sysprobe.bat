dcc7cd94e7f4799f94a4c089cd1d13368e2cff0f
Bryce Kahle <bryce.kahle@datadoghq.com>
2021-06-25 11:52:27 -0700

    Fix artifact collection after sysprobe windows test build

--- tasks/winbuildscripts/sysprobe.bat
+++ tasks/winbuildscripts/sysprobe.bat
@@ -8,8 +8,13 @@ if not exist c:\mnt\ goto nomntdir
 if NOT DEFINED PY_RUNTIMES set PY_RUNTIMES=%~1
 
 mkdir \dev\go\src\github.com\DataDog\datadog-agent
-cd \dev\go\src\github.com\DataDog\datadog-agent
-xcopy /e/s/h/q c:\mnt\*.*
+if not exist \dev\go\src\github.com\DataDog\datadog-agent exit /b 2
+cd \dev\go\src\github.com\DataDog\datadog-agent || exit /b 3
+xcopy /e/s/h/q c:\mnt\*.* || exit /b 4
 
 
-Powershell -C "c:\mnt\tasks\winbuildscripts\sysprobe.ps1"
+Powershell -C "c:\mnt\tasks\winbuildscripts\sysprobe.ps1" || exit /b 5
+
+REM copy resulting packages to expected location for collection by gitlab.
+if not exist c:\mnt\test\kitchen\site-cookbooks\dd-system-probe-check\files\default\ mkdir c:\mnt\test\kitchen\site-cookbooks\dd-system-probe-check\files\default\ || exit /b 6
+xcopy /e/s/q \dev\go\src\github.com\DataDog\datadog-agent\test\kitchen\site-cookbooks\dd-system-probe-check\files\default\*.* c:\mnt\test\kitchen\site-cookbooks\dd-system-probe-check\files\default\ || exit /b 7
